9caec885a4feda92fac12fb2aea930c615209732
Khaleel Lee <kbasha968@gmail.com>
2020-12-27 23:05:45 +0530

    Google Authentication Complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Imessage from "./Components/Imessage/Imessage";
+import Login from "./Components/Login/Login";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebaseConfig";
 
 function App() {
-	return (
-		<div className="app">
-			<Imessage />
-		</div>
-	);
+	const user = useSelector(selectUser);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		auth.onAuthStateChanged((authuser) => {
+			if (authuser) {
+				// user logged in
+				dispatch(
+					login({
+						uid: authuser.uid,
+						photo: authuser.photoURL,
+						email: authuser.email,
+						displayName: authuser.displayName,
+					})
+				);
+			} else {
+				//user logged out
+				dispatch(logout());
+			}
+		});
+	}, []);
+
+	return <div className="app">{user ? <Imessage /> : <Login />}</div>;
 }
 
 export default App;
